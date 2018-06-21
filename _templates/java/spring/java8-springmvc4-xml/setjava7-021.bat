dbbab2e65a9a76df74554787d673f0e757d546c5
Georgg <george.g@list.ru>
2018-06-21 10:30:56 +0300

    hibernate

--- _templates/java/spring/java8-springmvc4-xml/setjava7-021.bat
+++
@@ -1,3 +0,0 @@
-set "JAVA_HOME=C:\Program Files\Java\jdk1.7.0_21\"
-echo %JAVA_HOME%
-set "Path=%JAVA_HOME%\bin;%Path%;"
--- _templates/java/spring/java8-springmvc4-xml/setjava8-171.bat
+++
@@ -1,3 +0,0 @@
-set "JAVA_HOME=C:\Program Files\Java\jdk1.8.0_171"
-echo %JAVA_HOME%
-set "Path=%JAVA_HOME%\bin;%Path%;"
