785eea015a228eeae5c34f8748b866d6574433d7
Georgg <george.g@list.ru>
2018-06-21 05:11:45 +0300

    java10 template

---
+++ _templates/java/java10-template/src/main/java/geoorg/App.java
@@ -0,0 +1,14 @@
+package geoorg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        var s1 = "Hello Java 10";
+        System.out.println(s1);
+    }
+}
