d7f0789f6f1f2bf6e9be00fcfe4d9d95c24baa54
Nick Podlesny <podlesnyy.nikita@gmail.com>
2018-11-28 08:35:35 +0300

    nov27exception

--- video-lectures/ocp/src/main/java/geoorg/nov21excaption/Car.java
+++ video-lectures/ocp/src/main/java/geoorg/nov21excaption/Car.java
@@ -10,47 +10,70 @@ public class Car implements AutoCloseable {
     }
 
     public Car() {
-        System.out.println("Car is created");
-        this.number = 0;
+        this(0);
     }
 
     public void start() throws Exception {
         System.out.println("Car " + number + " is started");
-        //throw  new Exception("Car cannot be started");
+        throw new Exception("Car cannot be started");
     }
 
     @Override
-    public void close() throws Exception {
+    public void close() {
         System.out.println("Car " + number + " is closed");
-        throw new Exception("Car " + number + " cannot e closed");
+        //throw new Exception("Car " + number + " cannot e closed");
     }
 
-    public void foo() {
-        foo();
-    }
-
-    static Car car0 = null;
-    public static void main(String[] args) {
-        Car car2 = null;
+    public static void foo() {
         try (Car car1 = new Car(1)) {
-            car1.close();
-            car1.start();
+            throw new IllegalStateException("some problem");
+        } finally {
+            throw new RuntimeException("problem-2");
+        }
+    }
 
-            //car1 = new Car();
-            //Car car3 = new Car(3);
-            //car1.start();
-            //throw new Exception("try excaption");
+
+  //  static Car car0 = null;
+
+
+    public static  int square(int a) {
+        assert a > 0;
+        return a * a * a;
+    }
+
+    public static void main(String[] args) {
+
+        try (Car car1 = null) {
+
+            System.out.println("try section");
+        }
+        System.out.println("after try");
+
+
+/*
+        try {
+            foo();
         } catch (Exception e) {
             System.out.println("caught: "+ e.getMessage());
             for (Throwable t: e.getSuppressed())
+                System.out.println("suppresed: "+t.getMessage());
+        }
+
+        try (Car car1 = new Car()) {
+            car1.start();
+        } catch (Exception e) {
+            System.out.println("caught: "+ e.getMessage());
+
+            for (Throwable t: e.getSuppressed())
                  System.out.println("suppresed: "+t.getMessage());
+
         } finally {
             System.out.println("finally block");
             //throw new RuntimeException("finally excatpion");
         }
 
         System.out.println("after try");
-
+*/
     }
 
 }
